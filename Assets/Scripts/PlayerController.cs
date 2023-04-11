3217b99cd53477abd850e56fbf0ef71863644967
noahg957 <noahgluck@gmail.com>
2023-04-11 12:38:33 +0200

    basic move and blocks

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 100f;   // speed of movement
+    public float gridSize = 1f;    // size of the grid
+    public float jumpForce = 7f;   // force of jump
+
+    public float rateOfMove = 5;
+    private float lastMove = 0;
+
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(h, 0, v).normalized;
+
+        // Rotate movement vector to match camera angle
+        //direction = Quaternion.Euler(45, 0, 0) * direction;
+
+        // Calculate movement vector
+        Vector3 movement = direction * gridSize;
+
+        // Move character
+        if ((lastMove + 1 / rateOfMove) < Time.time)
+        {
+            lastMove = Time.time;
+            rb.MovePosition(transform.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        }
+
+        // Jump if space bar is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
